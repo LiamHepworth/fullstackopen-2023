17e0661d68e354afe5cd0cb8e620768b25e128e4
LiamHepworth <Liammoses1@gmail.com>
2023-04-23 16:14:55 +0100

    2.5 Extracting to modules

---
+++ coursework/part2/courseinfo-cont/src/components/Course.jsx
@@ -0,0 +1,35 @@
+const Header = ( {course} ) => {
+    return ( 
+      <h1>{course.name}</h1>
+    );
+}
+  
+const Part = ( {part, exercise} ) => {
+    return ( 
+      <p>
+        {part}: {exercise}
+      </p>
+    );
+}
+
+const Course = ( {courses} ) => {
+    return ( 
+        <>
+            {courses.map((course) => (
+            <div key={course.id}>
+                <Header course={course}></Header>
+                <ul>
+                {course.parts.map((c) =>
+                    <li key={c.id}>
+                    <Part part={c.name} exercise={c.exercises}></Part>
+                    </li>
+                )}
+                </ul>
+                <p><strong>Total Excercises: {course.parts.map((c) => c.exercises).reduce((a, c) => a + c)}</strong></p>
+            </div>
+            ))}
+        </>
+    );
+};
+  
+export default Course;

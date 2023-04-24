1e8523f95c4a99b35ba56ab6a5ea88c0b637b7a6
LiamHepworth <Liammoses1@gmail.com>
2023-04-24 09:50:10 +0100

    2.6 -  Step 1 - Adding Names

--- coursework/part2/phonebook/src/App.jsx
+++ coursework/part2/phonebook/src/App.jsx
@@ -6,19 +6,31 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addNewContact = (event) => {
+    event.preventDefault()
+
+    const newContact = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(newContact))
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
+      <form onSubmit={addNewContact}>
+        <div>name: <input value={newName} onChange={e => setNewName(e.target.value)}/> </div>  
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }

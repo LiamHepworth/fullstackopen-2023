e743158b58a5875ec4f1387a886b4a242b3adfd8
LiamHepworth <Liammoses1@gmail.com>
2023-04-24 13:15:56 +0100

    2.8 Add phone numbers

--- coursework/part2/phonebook/src/App.jsx
+++ coursework/part2/phonebook/src/App.jsx
@@ -2,22 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Liam Moses' },
+    { name: 'Arto Hellas',
+      number: 394453235235,
+    },
   ]) 
+  
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNewContact = (event) => {
     event.preventDefault()
 
     const newContact = {
       name: newName,
+      number: newNumber,
     }
 
     if(persons.map((person) => person.name).includes(newName)){
       alert(`${newName} already exists within contact list`)
     } else {
-      setPersons(persons.concat(newContact))      
+      setPersons(persons.concat(newContact))
     }
   };
 
@@ -25,15 +29,17 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewContact}>
-        <div>name: <input value={newName} onChange={e => setNewName(e.target.value)}/> </div>  
+        <div>name: <input value={newName} onChange={e => setNewName(e.target.value)}/> </div>
+        <div>number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/></div>  
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+
+      <h2>Contacts</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name}: {person.number}</li>
         ))}
       </ul>
     </div>

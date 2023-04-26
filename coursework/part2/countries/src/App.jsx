ad92dc7d641bc9e74099f8abcf3ef23360fda604
LiamHepworth <Liammoses1@gmail.com>
2023-04-26 16:38:46 +0100

    Country search working

--- coursework/part2/countries/src/App.jsx
+++ coursework/part2/countries/src/App.jsx
@@ -1,20 +1,10 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-  //use useEffect() to query the API each time the search field is updated??
-  //loop through name values of API
-  //check if countryName.includes(search) - if true {
-  //   return the first 10 countries to match search query
-  // }
-  //When only one name matches, get all country data for it
-
-  //request.data[i].name.official
-  //use filter to filter against search query
-
 function App() {
 
   const [search, setSearch] = useState('')
-  const [countries, setCountries] = useState(["switzerland", "swiss"])
+  const [countries, setCountries] = useState([])
 
   function handleSearch(e){
     const currentSearch = e.target.value;
@@ -22,22 +12,11 @@ function App() {
   }
 
   useEffect(() => {
-    console.log(search);
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((request) => {
-        // console.log(request.data.filter(country => country.name.official.includes(search)))
-        const validCountries = request.data.filter(country => country.name.official.includes(search))
-        console.log(validCountries)
-
-        if(validCountries.length <= 10){
-          setCountries(validCountries)
-          console.log("less than ten")
-          console.log(validCountries[0].name.official)
-        } else {
-          setCountries(["too many"])
-          console.log("more than ten")
-        }
+        const validCountries = request.data.filter(country => country.name.official.toUpperCase().includes(search.toUpperCase()))
+        setCountries(validCountries)
       })
   }, [search]);
 
@@ -51,15 +30,17 @@ function App() {
           }/>
       </form>
 
-      {/* {countries.length < 10 ? 
-        <ul>
-          {countries.map((country) => (
-            <li>{country.name.official}</li>
-          ))}
-        </ul>
-        :
-        <span>too many countries</span>
-      } */}
+        <div>
+        {countries.length < 10 ? 
+          <ul>
+            {countries.map((country) => (
+              <li key={countries.indexOf(country)}>{country.name.common}</li>
+            ))}
+          </ul>
+          :
+          <span>Too many countries, refine search</span>
+        }
+        </div>
 
     </div>
   )
